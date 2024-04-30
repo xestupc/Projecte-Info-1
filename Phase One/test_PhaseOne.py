217ca7b532f95f421071faef598b089c520ea87e
xestupc <xavier.estruga@estudiantat.upc.edu>
2024-04-30 22:09:05 +0200

    Format Changes, per fer el test phase 1 final a posteriori

---
+++ Phase One/test_PhaseOne.py
@@ -0,0 +1,6 @@
+
+# Main program for Phase 1
+# Additionally, write a test program for the class Graph test_graph.py using as a base the program
+# given below. Look at the function that generates the graph (crearGrafoEjemplo), it adds the nodes
+# with their names and coordinates, then it adds the segments that connect the nodes. Extend to test
+# the error conditions.
