5da672204e9232c86ab3ac50a1ff7a3aa1b16b9b
noahzafraupc <noah.zafra@estudiantat.upc.edu>
2024-05-06 14:21:36 +0200

    no message

---
+++ test_segment.py
@@ -0,0 +1,18 @@
+from segment import Segment
+import node
+
+n1 = node.Node("A", 4, 25)
+n2 = node.Node("B", 9, 30)
+n3 = node.Node("C", 11, 40)
+
+print("The distance between n1 and n2 is: ")
+Segment(n1, n2)
+print("The distance between n2 and n3 is: ")
+Segment(n2, n3)
+
+node.AddNeighbor(n1, n2)
+node.AddNeighbor(n2, n3)
+
+print(n1.__dict__)
+print(n2.__dict__)
+print(n3.__dict__)
