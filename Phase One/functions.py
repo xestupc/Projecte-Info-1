09fa3c3b03bc021e85d6e9b9f0f68c29d9efecdc
Hafssa Kamli <hafssa.kamli@estudiantat.upc.edu>
2024-05-07 12:10:44 +0200

    no message

--- Phase One/functions.py
+++ Phase One/functions.py
@@ -1,46 +1,38 @@
+class Node():
+   def __init__(self, x, y, vecinos_lista):
+       self.x = x #X coordinate
+       self.y = y #Y coordinate
+       self.veins = vecinos_lista
 
-class Nodo():
-    def __init__(self, x, y, vecinos_lista):
-        self.x = x
-        self.y = y
-        self.veins = vecinos_lista
 
-node1 = Nodo(100, 200, [])
+node1 = Node(100, 200, [])
+
+
 
 
 def addNeighbor(n1, n2):
-    n1.veins.append(n2)
-    if n2 not in nodo1.veins:
-       nodo1.veins.append(n2)
-
-
-    # while loop 
-
-    
-    # for 
-
-
-
-# poner condicional si n2 ya está en lista vecinos n1 
-# no volver a añadirlo
-
-
-# pistas: buscar syntax del if 
-# vas a tener que jugar con un bucle y los indices de una lista
-# ya te doy la lsita de vecinos n1, mirar si n2 lo añado o no.
-
+   if n2 not in n1.veins:
+   #if n2 is not in n1's neighbors list, it will be added. The program will return False
+       n1.veins.append(n2)
+       return False
+   #id n2 is already present in n1's neighbors list, the program will return True
+   else:
+       return True
+ 
 
 
 def distance (n1, n2):
-    distance = ((n2.x - n1.x)** 2 + (n2.y - n1.y)**2)**(1/2)
-    return distance
-nodo1= Nodo(2, 4, [])
-nodo2= Nodo(5, 6, [])
-
-nodo1.veins = [nodo3, nodo4, nodo5, nodo6, nodo7, nodo8, nodo9, nodo10]
+   #Calculates the Euclidean distance betwween two nodes
+   distance = ((n2.x - n1.x)** 2 + (n2.y - n1.y)**2)**(1/2)
+   return distance
 
 
+#examplee usage
+nodo1= Node(2, 4, [])
+nodo2= Node(5, 6, [])
 
+
+print(addNeighbor(nodo1, nodo2))
 print(f"\n Distancia =  {distance(nodo1, nodo2)}")
 
 
