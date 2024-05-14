d5b20cc01712fcf0b9348bbf385f5a6ca8c872b2
Hafssa Kamli <hafssa.kamli@estudiantat.upc.edu>
2024-05-14 13:49:24 +0200

    no message

---
+++ WORKING_COPY/AirSpace.py
@@ -0,0 +1,9 @@
+class AirSpace:
+    def __init__(self):
+        self.points =[]
+        self.segments = []
+        self.airports = []
+        self.sids = []
+        self.stars = []
+    def addPoint (air, point):
+        
