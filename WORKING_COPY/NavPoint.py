106a6426dfc10d5d498f5f05d69872e7971c54af
noahzafraupc <noah.zafra@estudiantat.upc.edu>
2024-05-14 13:36:08 +0200

    cambios 14/05

---
+++ WORKING_COPY/NavPoint.py
@@ -0,0 +1,14 @@
+
+
+class NavPoint():
+    def __init__(self, num, name, lat, lon):
+        self.num = num
+        self.name = name
+        self.lat = lat
+        self.lon = lon
+
+
+def NavPointFunc(num, name, lat, lon):
+    nav_point = NavPoint(num, name, lat, lon)
+    return nav_point
+
