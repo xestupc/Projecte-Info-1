19d0fe5325aa14e2a213f00ad5040fa03ed92c79
xestupc <xavier.estruga@estudiantat.upc.edu>
2024-05-20 23:33:25 +0200

    ?

--- Main/NO TOCAR -[Group13_INFO1_Projecte].py
+++ Main/NO TOCAR -[Group13_INFO1_Projecte].py
@@ -1,18 +1,27 @@
 
 # Llibreries externes
-
 import matplotlib as plt
 import math as math
 
 # Llibreries propies
 
+# Misc
 import UI_imprv as clr
+
+# Phase Zero
 import flightplan as fp
 import waypoint as wp
 
+# Phase One
 import graph as gr
-import node as nd
-import segment as seg
+import node as node
+import segment as segm
 
+# Phase Two
 import path as path
 
+
+# Phase Three
+
+
+# Phase Four
